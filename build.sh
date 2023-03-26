2dd04e53fce971df85b55f47f14d3a97fe9ff23b
gabrispada <io@gabriele.spadaro.name>
2023-03-26 19:54:07 +0200

    change docker name

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build -t eidos-service.di.unito.it/spadaro/compress-graph:latest . -f Dockerfile
-docker push eidos-service.di.unito.it/spadaro/compress-graph:latest
+docker build -t eidos-service.di.unito.it/spadaro/compress-graph-pytorch:latest . -f Dockerfile
+docker push eidos-service.di.unito.it/spadaro/compress-graph-pytorch:latest
